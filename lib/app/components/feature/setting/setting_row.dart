e7cd888c8d80af080a52ccae0d67549bf07946f3
Thang Nguyen <thangnm2k@gmail.com>
2024-07-12 20:12:00 +0700

    Update: UI SystemRow

--- lib/app/components/feature/setting/setting_row.dart
+++ lib/app/components/feature/setting/setting_row.dart
@@ -15,80 +15,88 @@ class SettingRow extends StatelessWidget {
     required this.icon,
     required this.title,
     required this.body,
-    required this.onTap,
+    this.onTap,
   });
 
   final IconData icon;
   final String title;
   final String body;
-  final Function onTap;
+  final Function? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => onTap.call(),
-      child: Row(
-        children: [
-          Gap(AppThemeExt.of.dimen(2)),
-          SizedBox(
-            width: settingIconAreaSize,
-            child: Center(
-              child: Container(
-                width: settingIconBackgroundSize,
-                height: settingIconBackgroundSize,
-                decoration: BoxDecoration(
-                  color: backgroundDimColor,
-                  borderRadius: AppThemeExt.of.borderRadius(2),
-                ),
-                child: Icon(icon),
+    return onTap == null
+        ? content(context)
+        : InkWell(
+            onTap: () => onTap?.call(),
+            child: content(context),
+          );
+  }
+
+  Widget content(BuildContext context) {
+    return Row(
+      children: [
+        Gap(AppThemeExt.of.dimen(2)),
+        settingIcon(),
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              divider,
+              Gap(AppThemeExt.of.dimen(2)),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  settingTextInfo(context),
+                  Gap(AppThemeExt.of.dimen(2)),
+                  if (onTap != null) const FaIcon(FontAwesomeIcons.chevronRight),
+                ],
               ),
-            ),
+              Gap(AppThemeExt.of.dimen(2)),
+              divider,
+            ],
           ),
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                divider,
-                Gap(AppThemeExt.of.dimen(2)),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            title,
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .copyWith(
-                                  color: AppColors.of.mainTextColor,
-                                ),
-                          ),
-                          Text(
-                            body,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                  color: AppColors.of.mainTextColor,
-                                ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Gap(AppThemeExt.of.dimen(2)),
-                    const FaIcon(FontAwesomeIcons.chevronRight),
-                  ],
+        ),
+        Gap(AppThemeExt.of.dimen(4)),
+      ],
+    );
+  }
+
+  Widget settingIcon() {
+    return SizedBox(
+      width: settingIconAreaSize,
+      child: Center(
+        child: Container(
+          width: settingIconBackgroundSize,
+          height: settingIconBackgroundSize,
+          decoration: BoxDecoration(
+            color: backgroundDimColor,
+            borderRadius: AppThemeExt.of.borderRadius(2),
+          ),
+          child: Icon(icon),
+        ),
+      ),
+    );
+  }
+
+  Widget settingTextInfo(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: AppColors.of.mainTextColor,
                 ),
-                Gap(AppThemeExt.of.dimen(2)),
-                divider,
-              ],
-            ),
           ),
-          Gap(AppThemeExt.of.dimen(4)),
+          Text(
+            body,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: AppColors.of.mainTextColor,
+                ),
+          ),
         ],
       ),
     );
